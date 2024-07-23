ed91b813a0d374849e9cde5070a74676cdd29713
Jaime Calderon <Jaimecalderon@MacBook-Air-de-Jaime.local>
2024-07-23 17:59:21 +0200

    V2

--- LAB14.sql
+++ LAB14.sql
@@ -1,15 +1,21 @@
 -- Active: 1721291050538@@127.0.0.1@3306@sakila
 USE sakila;
 --1)
-SELECT MAX(length) AS 'MAX Duration', MIN(length) AS 'MIN Duration' FROM film;
+SELECT MAX(rental_duration) AS max_duration, MIN(rental_duration) AS min_duration
+FROM film;
 SELECT ROUND((AVG(length)), 0) AS 'AVG Duration in minutes' FROM film;
 SELECT TIME_FORMAT(SEC_TO_TIME(AVG(length)*60), '%H:%i') AS 'AVG duration in hours and minutes' FROM film;
+SELECT FLOOR(AVG(length) / 60) AS hours, ROUND(AVG(length) % 60) AS minutes
+FROM film;
 --2)
 SELECT MIN(rental_date) FROM rental;
 SELECT MAX(rental_date) FROM rental;
 SELECT DATEDIFF(MAX(rental_date), MIN(rental_date)) FROM rental;
 SELECT *, CONVERT(rental_date,date) as 'Month' FROM rental;
 SELECT *, DATE_FORMAT(rental_date,'%M') AS 'MONTH',DATE_FORMAT(rental_date,'%W') AS 'DAY' FROM rental LIMIT 20;
+SELECT *, DATE_FORMAT(rental_date, '%M') AS MONTH, DATE_FORMAT(rental_date, '%W') AS WEEKDAY
+FROM rental
+LIMIT 20;
 SELECT *, CASE WHEN DATE_FORMAT(rental_date,'%W') IN ('Monday','Tuesday','Wednesday','Thursday','Friday') THEN 'Workday' ELSE 'Weekend' END AS 'DAY TYPE' FROM rental LIMIT 20;
 --3) 
 SELECT title, rental_duration, IFNULL(rental_duration, 'Not Available') AS 'ERROR' FROM film ORDER BY title ASC;
